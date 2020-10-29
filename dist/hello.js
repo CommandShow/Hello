da903b2bf2f4f3cd2769ff88a55cee93d38042c4
Candison <kandisheng@163.com>
2020-10-29 12:33:11 +0000

    Build by GitHub Actions

---
+++ dist/hello.js
@@ -0,0 +1,31 @@
+const noColor = globalThis.Deno?.noColor ?? true;
+let enabled = !noColor;
+function code(open, close) {
+    return {
+        open: `\x1b[${open.join(";")}m`,
+        close: `\x1b[${close}m`,
+        regexp: new RegExp(`\\x1b\\[${close}m`, "g")
+    };
+}
+function run(str, code1) {
+    return enabled ? `${code1.open}${str.replace(code1.regexp, code1.open)}${code1.close}` : str;
+}
+function green(str) {
+    return run(str, code([
+        32
+    ], 39));
+}
+function brightBlack(str) {
+    return run(str, code([
+        90
+    ], 39));
+}
+function clampAndTruncate(n, max = 255, min = 0) {
+    return Math.trunc(Math.max(Math.min(n, max), min));
+}
+const ANSI_PATTERN = new RegExp([
+    "[\\u001B\\u009B][[\\]()#;?]*(?:(?:(?:[a-zA-Z\\d]*(?:;[-a-zA-Z\\d\\/#&.:=?%@~_]*)*)?\\u0007)",
+    "(?:(?:\\d{1,4}(?:;\\d{0,4})*)?[\\dA-PR-TZcf-ntqry=><~]))", 
+].join("|"), "g");
+const name = Deno.args.join(" ");
+console.log("Hello", green(name));
