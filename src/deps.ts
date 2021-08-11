a517c87cb13a571ee163637932d103b4c56f7d87
Candison <kandisheng@163.com>
2021-08-11 17:43:53 +0800

    Update deps.ts

--- src/deps.ts
+++ src/deps.ts
@@ -1,1 +1,1 @@
-export { green } from "https://cdn.jsdelivr.net/gh/denoland/deno_std@0.103.0/fmt/colors.ts";
+export { green } from "https://cdn.jsdelivr.net/gh/denoland/deno_std@0.104.0/fmt/colors.ts";
