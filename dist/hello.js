83c6f0b0f9f70914a9090a4e84abf6c0486ff8bf
Candison <kandisheng@163.com>
2024-06-14 06:25:37 +0000

    Build by GitHub Actions

--- dist/hello.js
+++ dist/hello.js
@@ -22,7 +22,7 @@ function green(str) {
 }
 new RegExp([
     "[\\u001B\\u009B][[\\]()#;?]*(?:(?:(?:(?:;[-a-zA-Z\\d\\/#&.:=?%@~_]+)*|[a-zA-Z\\d]+(?:;[-a-zA-Z\\d\\/#&.:=?%@~_]*)*)?\\u0007)",
-    "(?:(?:\\d{1,4}(?:;\\d{0,4})*)?[\\dA-PR-TZcf-nq-uy=><~]))"
+    "(?:(?:\\d{1,4}(?:;\\d{0,4})*)?[\\dA-PR-TXZcf-nq-uy=><~]))"
 ].join("|"), "g");
 const name = Deno.args.join(" ");
 console.log("Hello", green(name));
